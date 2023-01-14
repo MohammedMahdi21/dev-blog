3a0302d323eb9a8493a1ad95a1c9b8f0b188892d
Mohammed Mahdi <mmd.mahdi21@gmail.com>
2023-01-14 14:36:10 +0200

    add routes to the pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,40 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Single from "./pages/Single"
+import Write from "./pages/Write"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This is Home Page!</div>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/single",
+    element: <Single/>,
+  },
+  {
+    path: "/write",
+    element: <Write/>,
+  },
+]);
+
+
 function App() {
   return (
     <div>
-      Hello World
+      <RouterProvider router={router}/>
     </div>
   );
 }
