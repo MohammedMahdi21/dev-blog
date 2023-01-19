2b4c827680c0e0326c9568df902aeae70014b5fb
Mohammed Mahdi <mmd.mahdi21@gmail.com>
2023-01-19 15:29:39 +0200

    organize code

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -7,35 +7,22 @@ import Delete from "../img/delete.png"
 
 const Single = () => {
   return (
-
     <div className="single">
-
       <div className="content">
-
         <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
-
         <div className="user">
-
           <img src="https://uploads.toptal.io/profile_photo/image/user/714881/large_45b43ef21e76b273e3e72ffea850f56e.jpg" alt="" />
-
           <div className="info">
-
             <span>Moe</span>
             <p>Posted 2 days ago</p>
-
           </div>
-
           <div className="edit">
-
             <Link to={`/write?edit=2`}>
               <img src={Edit} alt="" />
             </Link>
             <img src={Delete} alt="" />
-
           </div>
-
         </div>
-
         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
         <p>
           <p>
@@ -45,11 +32,8 @@ const Single = () => {
             He focuses on data-driven design products and brand strategies that delight customers. He has changed the way organizations solve problems as well as produced world-class B2B and B2C interfaces.
           </p>
         </p>
-
       </div>
-
       <Menu />
-
     </div>
   )
 }
