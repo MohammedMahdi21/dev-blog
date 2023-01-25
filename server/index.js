2fb795394db18cd78d5df34de9ce37f8a717969f
Mohammed Mahdi <mmd.mahdi21@gmail.com>
2023-01-25 17:35:17 +0200

    add cors to the app

--- server/index.js
+++ server/index.js
@@ -2,14 +2,15 @@ import express from "express";
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
 import postRoutes from "./routes/posts.js"
+import cors from "cors"
 
 const app = express()
+app.use(cors());
 
 app.use(express.json())
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
-
 
 app.listen(8800, () => {
   console.log("Connected on port 8800!")
