e328185ea4c62e799861e2363c31a56fde1198eb
Mohammed Mahdi <mmd.mahdi21@gmail.com>
2023-01-26 22:31:45 +0200

    add cookie parser

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
 import postRoutes from "./routes/posts.js"
+import cookieParser from "cookie-parser";
 import cors from "cors"
 
 const app = express()
 app.use(cors());
 
 app.use(express.json())
+app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
