c852217a7e4420e141671bbd13956585d3234667
Mohammed Mahdi <mmd.mahdi21@gmail.com>
2023-01-26 22:33:30 +0200

    add login auth. and fix register auth.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
 import { db } from "../db.js"
 import bcrypt from "bcryptjs"
-import { json } from "express"
+import  jwt  from "jsonwebtoken"
+
 export const register = (req, res) => {
 
   //Check existing user
@@ -15,7 +16,7 @@ export const register = (req, res) => {
     //Hash the password and create a user
 
     const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync("B4c0/\/", salt);
+    const hash = bcrypt.hashSync(req.body.password, salt);
 
     const q = "INSERT INTO users(`username`,`email`,`password`) VALUES (?)"
     const valuse = [
@@ -32,6 +33,29 @@ export const register = (req, res) => {
 
 }
 export const login = (req, res) => {
+
+  //Check user
+
+  const q = "SELECT * FROM users WHERE username = ?"
+
+  db.query(q,[req.body.username],(err, data) =>{
+    if (err) return res.json(err);
+    if(data.length === 0) return res.status(404).json("User not found!");
+
+    //Check password
+
+    const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
+
+    if(!isPasswordCorrect) return res.status(400).json("Wrong username or password!")
+
+    const token = jwt.sign({id:data[0].id}, "jwtkey")
+
+    const {password, ...other} = data[0]
+    res.cookie("access_token", token, {
+      httpOnly:true
+    }).status(200).json(other)
+
+  })
 
 }
 export const logout = (req, res) => {
