8fc80d478b9fbc05a0138e152718115d5850d9de
Mohammed Mahdi <mmd.mahdi21@gmail.com>
2023-01-26 23:44:01 +0200

    update

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -14,7 +14,7 @@ export const AuthContextProvider = ({ children }) => {
   };
 
   const logout = async (inputs) => {
-    const res = await axios.post('/auth/logout', inputs);
+    await axios.post('/auth/logout', inputs);
 
     setCurrentUser(null)
   };
