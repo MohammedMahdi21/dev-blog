fdef256f9162578998cd4bf1c0985d25565bb4fc
Mohammed Mahdi <99882304+MohammedMahdi21@users.noreply.github.com>
2023-02-01 01:48:26 +0200

    add link to the logo that take you to home page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,7 +12,11 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <div className="container">
-        <div className="logo"><img src={Logo} alt="" /></div>
+        <div className="logo">
+          <Link to="/">
+            <img src={Logo} alt="" />
+          </Link>
+        </div>
         <div className="links">
           <Link className='link' to="/?cat=art">
             <h6>ART</h6>
