4f3eff7759b4c6622b658cbeb32a7c092351af3b
Mohammed Mahdi <99882304+MohammedMahdi21@users.noreply.github.com>
2023-02-02 08:10:16 +0200

    fetch single page post

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,40 +1,53 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Menu from "../components/Menu"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import "./single.scss"
 import Edit from "../img/edit.png"
 import Delete from "../img/delete.png"
+import axios from "axios"
+import moment from "moment"
+import { useContext } from "react"
+import { AuthContext } from "../context/authContext"
 
 const Single = () => {
+  const [post, setPost] = useState({});
+
+  const location = useLocation();
+
+  const postId = location.pathname.split("/")[2];
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`)
+        setPost(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData()
+  }, [postId])
   return (
     <div className="single">
       <div className="content">
-        <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        <img src={post?.img} alt="" />
         <div className="user">
           <img src="https://uploads.toptal.io/profile_photo/image/user/714881/large_45b43ef21e76b273e3e72ffea850f56e.jpg" alt="" />
           <div className="info">
-            <span>Moe</span>
-            <p>Posted 2 days ago</p>
+            <span>{post.username}</span>
+            <p>Posted {moment(post.data).fromNow()}</p>
           </div>
-          <div className="edit">
+          {currentUser.username === post.username && <div className="edit">
             <Link to={`/write?edit=2`}>
               <img src={Edit} alt="" />
             </Link>
             <img src={Delete} alt="" />
-          </div>
+          </div>}
         </div>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
-        <p>
-          <p>
-            Victor is an award-winning designer and brand strategist with a decade of experience that has worked with various organizations, from pre-funding startups to organizations such as Siemens, Santander Bank, and Save the Children.
-            <br />
-            <br />
-            He focuses on data-driven design products and brand strategies that delight customers. He has changed the way organizations solve problems as well as produced world-class B2B and B2C interfaces.
-            <br />
-            <br />
-            He focuses on data-driven design products and brand strategies that delight customers. He has changed the way organizations solve problems as well as produced world-class B2B and B2C interfaces.
-          </p>
-        </p>
+        <h1>{post.title}</h1>
+        {post.desc}
       </div>
       <Menu />
     </div>
