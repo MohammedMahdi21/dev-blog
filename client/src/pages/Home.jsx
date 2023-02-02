acfedaad4b1fe56f0520cb18bfd17e9d2e0a38b9
Mohammed Mahdi <99882304+MohammedMahdi21@users.noreply.github.com>
2023-02-02 08:09:17 +0200

    fetch posts by category

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from "react"
 import { useEffect } from "react"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import axios from "axios"
 import "./home.scss"
 const Home = () => {
 
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
+
+  const cat = useLocation().search;
+
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get("/posts")
+        const res = await axios.get(`/posts/${cat}`)
         setPosts(res.data);
       } catch (err) {
         console.log(err);
       }
     };
     fetchData()
-  }, [])
+  }, [cat])
   // posts to test:
   // const posts = [
   //   {
