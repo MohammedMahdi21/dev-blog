be302a6ec2ceaaad5e84e47267292d80dbc95aed
Mohammed Mahdi <99882304+MohammedMahdi21@users.noreply.github.com>
2023-02-03 04:55:02 +0200

    create deletepost that will handle checking token the delete post that belong to the user

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,4 +1,5 @@
 import { db } from "../db.js"
+import jwt from "jsonwebtoken";
 
 export const getPosts = (req, res) => {
   const q = req.query.cat ? "SELECT * FROM posts WHERE cat=?" : "SELECT * FROM posts";
@@ -14,7 +15,7 @@ export const getPost = (req, res) => {
   const q = "SELECT `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`, `date` FROM users AS u JOIN posts AS p ON u.id = p.uid WHERE p.id = ?"
 
   db.query(q, [req.params.id], (err, data) => {
-    if (err) return res.json(err);
+    if (err) return res.status(500).json(err);
 
     return res.status(200).json(data[0]);
   })
@@ -25,7 +26,23 @@ export const addPost = (req, res) => {
 };
 
 export const deletePost = (req, res) => {
-  res.json("from controller")
+  const token = req.cookies.access_token
+  if (!token) return res.status(401).json("Not Authenticated!");
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+
+    if (err) return res.status(403).json("Token is not valid!")
+
+    const postId = req.params.id;
+    const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?";
+
+    db.query(q, [postId, userInfo.id], (err, data) => {
+      if (err) return res.status(403).json("You can delete only your posts!");
+
+      return res.json("post has been deleted!")
+    })
+
+  })
 };
 
 export const updatePost = (req, res) => {
