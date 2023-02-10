009bfb332c0b424f70c40e23a05cb7d523f1b786
Mohammed Mahdi <99882304+MohammedMahdi21@users.noreply.github.com>
2023-02-10 21:37:52 +0200

    setup multer to upload files to our server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
 import express from "express";
-import authRoutes from "./routes/auth.js"
-import userRoutes from "./routes/users.js"
-import postRoutes from "./routes/posts.js"
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import multer from "multer";
+import cors from "cors";
 
 const app = express()
 app.use(cors());
 
 app.use(express.json())
 app.use(cookieParser())
+
+const upload = multer({ dest: 'uploads/' })
+app.post('/upload', upload.single('avatar'), function (req, res, next) {
+  res.status(200).json("Img has been uploaded.")
+})
+
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
