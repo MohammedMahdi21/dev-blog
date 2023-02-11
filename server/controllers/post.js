db94ec97a1fa6aada21d747517c3f7a19d4fab1f
Mohammed Mahdi <99882304+MohammedMahdi21@users.noreply.github.com>
2023-02-11 04:52:10 +0200

    create addPost and update post querys

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -12,7 +12,9 @@ export const getPosts = (req, res) => {
 };
 
 export const getPost = (req, res) => {
-  const q = "SELECT `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`, `date` FROM users AS u JOIN posts AS p ON u.id = p.uid WHERE p.id = ?"
+  const q =
+    "SELECT p.id, `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`,`date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
+
 
   db.query(q, [req.params.id], (err, data) => {
     if (err) return res.status(500).json(err);
@@ -22,7 +24,29 @@ export const getPost = (req, res) => {
 };
 
 export const addPost = (req, res) => {
-  res.json("from controller")
+  const token = req.cookies.access_token
+  if (!token) return res.status(401).json("Not Authenticated!");
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+
+    if (err) return res.status(403).json("Token is not valid!")
+
+    const q = "INSERT INTO posts(`title`, `desc`, `img`, `date`, `cat`, `uid`) VALUES (?)"
+
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.img,
+      req.body.date,
+      req.body.cat,
+      userInfo.id
+    ]
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.json("Post has been created.");
+    });
+  });
 };
 
 export const deletePost = (req, res) => {
@@ -46,5 +70,26 @@ export const deletePost = (req, res) => {
 };
 
 export const updatePost = (req, res) => {
-  res.json("from controller")
+  const token = req.cookies.access_token
+  if (!token) return res.status(401).json("Not Authenticated!");
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+
+    if (err) return res.status(403).json("Token is not valid!")
+
+    const postId = req.params.id;
+    const q = "UPDATE posts SET `title`=?, `desc`=?, `img`=?`, `cat`=? WHERE `id` = ? AND `uid` = ? "
+
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.img,
+      req.body.cat,
+    ]
+
+    db.query(q, [...values, postId, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.json("Post has been updated.");
+    })
+  });
 };
