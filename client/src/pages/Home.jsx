77d0be02ea8d28bb7d3e3a5184be48cc02b08b2a
Mohammed Mahdi <99882304+MohammedMahdi21@users.noreply.github.com>
2023-02-12 11:55:33 +0200

    fix bug

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,7 +20,6 @@ const Home = () => {
     };
     fetchData()
   }, [cat])
-  // posts to test:
   // const posts = [
   //   {
   //     id: 1,
@@ -48,6 +47,11 @@ const Home = () => {
   //   },
   // ];
 
+  const getText = (html) => {
+    const doc = new DOMParser().parseFromString(html, "text/html")
+    return doc.body.textContent
+  }
+
   return (
     <div className="home">
       <div className="posts">
@@ -60,7 +64,7 @@ const Home = () => {
               <Link className="link" to={`/post/${post.id}`}>
                 <h1>{post.title}</h1>
               </Link>
-              <p>{post.desc}</p>
+              <p>{getText(post.desc)}</p>
               <button>Read More</button>
             </div>
           </div>
